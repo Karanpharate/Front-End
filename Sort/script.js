09586bb05cab4014a7470da7585065d26ac8dc8e
Karanpharate <43640225+Karanpharate@users.noreply.github.com>
2019-08-19 11:11:58 +0530

    adds responsive webpage assignment

--- Sort/script.js
+++ Sort/script.js
@@ -1,29 +1,32 @@
-const btn1 = document.getElementById('button1');
- btn2 = document.getElementById('button2'),
- array = [2, 4, 1, 3],
- num1 = document.getElementById("numberBox1"),
- num2 = document.getElementById("numberBox2"),
- num3 = document.getElementById("numberBox3"),
- num4 = document.getElementById("numberBox4");
-document.getElementById("numberBox1").innerHTML = array[0];
-document.getElementById("numberBox2").innerHTML = array[1];
-document.getElementById("numberBox3").innerHTML = array[2];
-document.getElementById("numberBox4").innerHTML = array[3];
-btn1.addEventListener('click',sortAsc = () => {
-  array.sort((a,b) => a-b);
-  num1.innerHTML = array[0];
-  num2.innerHTML = array[1];
-  num3.innerHTML = array[2];
-  num4.innerHTML = array[3];
-  document.getElementById('button1').classList.add('hide');
-  document.getElementById('button2').classList.remove('hide');
+const ascButton = document.getElementById('ascButton'),
+  descButton = document.getElementById('descButton'),
+  numArray = [2, 4, 1, 3],
+  firstBlock = document.getElementById('firstBlock'),
+  secondBlock = document.getElementById('secondBlock'),
+  thirdBlock = document.getElementById('thirdBlock'),
+  fourthBlock = document.getElementById('fourthBlock');
+
+firstBlock.innerHTML = numArray[0];
+secondBlock.innerHTML = numArray[1];
+thirdBlock.innerHTML = numArray[2];
+fourthBlock.innerHTML = numArray[3];
+
+ascButton.addEventListener('click', sortAsc = () => {
+  numArray.sort((a, b) => a - b);
+  firstBlock.innerHTML = numArray[0];
+  secondBlock.innerHTML = numArray[1];
+  thirdBlock.innerHTML = numArray[2];
+  fourthBlock.innerHTML = numArray[3];
+  ascButton.classList.add('hide');
+  descButton.classList.remove('hide');
 });
-btn2.addEventListener('click',sortDesc = () => {
-  array.sort((a,b) => b-a);
-  num1.innerHTML = array[0];
-  num2.innerHTML = array[1];
-  num3.innerHTML = array[2];
-  num4.innerHTML = array[3];
-  document.getElementById('button2').classList.add('hide');
-  document.getElementById('button1').classList.remove('hide');
+
+descButton.addEventListener('click', sortDesc = () => {
+  numArray.sort((a, b) => b - a);
+  firstBlock.innerHTML = numArray[0];
+  secondBlock.innerHTML = numArray[1];
+  thirdBlock.innerHTML = numArray[2];
+  fourthBlock.innerHTML = numArray[3];
+  descButton.classList.add('hide');
+  ascButton.classList.remove('hide');
 });
