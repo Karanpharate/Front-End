0557b149fa3a500410dce5adcecc63cd1fce7df8
Karanpharate <43640225+Karanpharate@users.noreply.github.com>
2019-08-19 10:39:24 +0530

    adds nodeJS assignment

---
+++ nodeJs/read.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+fs.writeFileSync('sample.txt','My name is Karan.');
+fs.readFile('sample.txt','UTF8',(err,content) => {
+  if(err) console.log(err);
+  console.log(content);
+});
