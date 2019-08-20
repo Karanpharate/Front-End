3813c2ee5b28bcc04d61f8911a61204cd2daa6ee
Karanpharate <43640225+Karanpharate@users.noreply.github.com>
2019-08-20 16:35:37 +0530

    code refactoring and validation

--- API/main.js
+++ API/main.js
@@ -1,16 +1,3 @@
-// fetch('https://reqres.in/api/products')
-//   .then((response) => response.json())
-//   .then((myJson) => {
-//     let parentElem = document.createElement("ul");
-//     parentElem.classList.add('productListing');
-//     document.body.appendChild(parentElem);
-//     myJson.data.map((item, key) => {
-//       let childElem = document.createElement("li");
-//       childElem.innerHTML = `<span class="test">${item.id}.</span> <a href="#">${item.name}</a> <span class="test">${item.year}</span> <span class="test">${item.color}</span>`;
-//       parentElem.appendChild(childElem);
-//     });
-//   });
-
 
 fetch('https://reqres.in/api/users')
   .then((response) => response.json())
